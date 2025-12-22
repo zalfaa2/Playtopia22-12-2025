a91d36ed3829e785ef076c546482e26186dba1e2
zalfaa2 <zalfahanifah770@gmail.com>
2025-12-22 13:42:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="ajel Cool App",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
