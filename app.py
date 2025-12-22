7ddf5514557e1ed39edea196ea5d03b764cff4a5
zalfaa2 <zalfahanifah770@gmail.com>
2025-12-22 13:40:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+# import streamlit as st
+
+# st.write("Hello, *Cintakuu!* :sunglasses:")
+
 import streamlit as st
 
-st.write("Hello, *CINTAKUUU!* :love:")
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
